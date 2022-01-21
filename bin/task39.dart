ccf196493e32f63575b94c634ff420312c47de3e
OutLaw371 <gadjimurad040203@gmail.com>
2022-01-21 21:02:19 +0300

    1

---
+++ bin/task39.dart
@@ -0,0 +1,51 @@
+// void main(){
+//   Character person = Character('Артём', 'мужской', 18, 70);
+//   person.info();
+//
+//   Character girl = Character.masha(18, 50);
+//   girl.info();
+// }
+//
+// class Character {
+//   String name;
+//   String gender;
+//   int age;
+//   int weight;
+//
+//   Character(this.name, this.gender, this.age, this.weight);
+//
+//   Character.masha(this.age, this.weight, {this.name = 'Маша', this.gender = 'женский'});
+//
+//   void info(){
+//     print('Это $name. Его пол $gender. Возраст $age. Вес $weight');
+//   }
+//
+//   @override
+//   String toString(){
+//     return 'Имя: $name \nПол: $gender \nВозраст: $age \nВес:$weight\n';
+//   }
+// }
+
+// void main(){
+//
+// }
+//
+// class Hero{
+//   String name;
+//   int health;
+//   int energy;
+//   int damage;
+//
+//   Hero(this.name, this.health, this.energy, this.damage);
+//
+//
+//   void attack(){
+//
+//   }
+//   void heal(){
+//
+//   }
+//   void eat(){
+//
+//   }
+// }
